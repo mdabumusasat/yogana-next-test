eddbd550de0883b107545e3b8e15f6def1bf1263
Abu Musa <96811902+mdabumusasat@users.noreply.github.com>
2025-12-11 16:59:26 +0600

    Update Funfact.tsx

--- src/app/components/elements/Funfact.tsx
+++ src/app/components/elements/Funfact.tsx
@@ -38,7 +38,7 @@ const Funfact = () => {
                 <div className="inner-box">
                 <div className="icon-box"><i className={`icon flaticon-${item.icon}`} /></div><br />
                 <div className="content-box">
-                    <div className="count-box"><span className="count-text"><CounterUp end={item.count} /></span>{item.countsymbol}</div>
+                    <div className="count-box"><span className="count-text"><CounterUp end={Number(item.count)} /></span>{item.countsymbol}</div>
                     <div className="counter-title">{item.title}</div>
                 </div>
                 </div>
